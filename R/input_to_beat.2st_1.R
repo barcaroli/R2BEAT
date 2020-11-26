73c462b253b0ad54732507661ccb0e8c925bd112
Giulio Barcaroli <gbarcaroli@gmail.com>
2020-11-26 10:14:16 +0100

    devtools

--- R/input_to_beat.2st_1.R
+++ R/input_to_beat.2st_1.R
@@ -8,7 +8,6 @@ input_to_beat.2st_1 <- function (RGdes, RGcal, id_PSU, id_SSU, strata_vars, targ
           domain_vars) 
 {
   if(!require(ReGenesees)){
-    require(devtools)
     devtools::install_github("DiegoZardetto/ReGenesees")
   }
   library(ReGenesees)
