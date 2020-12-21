d606bf6bb05f8a554298e4ab3a4f341928f5b7f0
Giulio Barcaroli <gbarcaroli@gmail.com>
2020-12-21 18:29:50 +0100

    check_input

--- R/check_input.R
+++ R/check_input.R
@@ -1,5 +1,9 @@
-check_input <- function(strata,des,strata_var_strata,strata_var_des) {
+check_input <- function(strata,
+                        des,
+                        strata_var_strata,
+                        strata_var_des) {
   a <- merge(strata[,c(strata_var_strata,"N")],des[,c(strata_var_des,"STRAT_MOS")])
+  a <- a[order(as.numeric(a$STRATUM)),]
   a$relative_difference <- round((a$STRAT_MOS - a$N) / a$STRAT_MOS,3)
   colnames(a)[2:3] <- c("N_in_strata","N_in_PSUs")
   cat("\n--------------------------------------------------")
