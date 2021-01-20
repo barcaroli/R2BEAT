099bfe4b6850d131810f64c7e1cc9faea5a50cc7
Giulio Barcaroli <gbarcaroli@gmail.com>
2021-01-20 20:01:22 +0100

    select_SSU

--- NEWS.md
+++ NEWS.md
@@ -1,16 +1,22 @@
 
 <!-- NEWS.md is generated from NEWS.Rmd. Please edit NEWS.Rmd file -->
 
+# R2BEAT 1.0.3
+
+## Major changes
+
+-   Added a function (‘select\_SSU’) for the selection of Secondary
+    Stage Units
+
 # R2BEAT 1.0.2
 
 ## Major changes
 
-  - Added two functions (‘input\_to\_beat.2st\_1’ and
-    ‘input\_to\_beat.2st\_2’) to prepare inputs for the allocation
-    step
+-   Added two functions (‘input\_to\_beat.2st\_1’ and
+    ‘input\_to\_beat.2st\_2’) to prepare inputs for the allocation step
 
-  - Added function ‘StratSel’ executing the PSU selection step
+-   Added function ‘StratSel’ executing the PSU selection step
 
-  - Fixed some bugs
+-   Fixed some bugs
 
 # R2BEAT 1.0.1
---
+++ R/select_SSU.R
@@ -0,0 +1,48 @@
+#-----------------------------------------
+# Function to select 2nd stage units (SSU)
+#-----------------------------------------
+
+select_SSU <- function(df,PSU_code,SSU_code,PSU_sampled,verbose=TRUE) {
+  select <- function(df,PSU_code,PSU_sampled) {
+    st <- paste0("PSU_ID <- df$",PSU_code,"[1]")
+    eval(parse(text=st))
+    st <- paste0("PSU_size <- PSU_sampled$PSU_final_sample_unit[PSU_sampled$PSU_ID==df$",PSU_code,"[1]]")
+    eval(parse(text=st))
+    s <- sample(1:nrow(df),size=PSU_size)
+    s <- df[s,]
+    st <- paste0("s$Pik <- PSU_sampled$Pik[PSU_sampled$PSU_ID==df$",PSU_code,"[1]]")
+    eval(parse(text=st))
+    s$Prob <- nrow(s) / nrow(df)
+    if (verbose==TRUE) {
+      cat("\nPSU = ",PSU_ID," *** Selected SSU = ",nrow(s))
+    }
+    return(s)
+  }
+  st <- paste0("frame <- frame[frame$",PSU_code," %in% PSU_sampled$PSU_ID, ]")
+  eval(parse(text=st))
+  st <- paste0("frame$municipality <- as.factor(frame$",PSU_code,")")
+  eval(parse(text=st))
+  st <- paste0("frame.split <- split(frame,frame$",PSU_code,",drop=TRUE)")
+  eval(parse(text=st))  
+  samp <- sapply(frame.split,function(df) select(df,PSU_code,PSU_sampled))
+  samp <- data.frame(t(samp))
+  st <- paste0("id_ind <- unlist(samp$",SSU_code,")")
+  eval(parse(text=st))   
+  Pik <- unlist(samp$Pik)
+  Prob <- unlist(samp$Prob)
+  st <- paste0("a <- as.data.frame(list(",SSU_code,",Pik,Prob))")
+  eval(parse(text=st)) 
+  st <- paste0("colnames(a) <- c('",SSU_code,"','Prob_1st','Prob_2st')")
+  eval(parse(text=st)) 
+  samp <- merge(pop,a)
+  samp$Prob_tot <- samp$Prob_1st * samp$Prob_2st
+  samp$weight <- 1/samp$Prob_tot
+  if (verbose==TRUE) {
+    cat("\n--------------------------------")
+    cat("\nTotal PSU = ",nrow(PSU_sampled))
+    cat("\nTotal SSU = ",nrow(samp))
+    cat("\n--------------------------------")
+  }  
+  return(samp)
+}
+
