17ed4d3ae2f249f56668ca0d62f36957162623ec
barcaroli <gbarcaroli@gmail.com>
2021-08-20 18:37:22 +0200

    prepareInput

--- R/prepareInputToAllocation2.R
+++ R/prepareInputToAllocation2.R
@@ -34,6 +34,17 @@ prepareInputToAllocation2 <- function(
   samp_frame$one <- 1
   psu <- eval(parse(text=paste0("aggregate(one~",id_PSU,"+",strata_vars,",data=samp_frame,FUN=sum)")))
   mos_var <- "one"
+  if (length(strata_vars) == 1) st <- paste0("samp_frame$stratum_new <- samp_frame$",strata_vars[1],"")
+  if (length(strata_vars) > 1) {
+    st <- NULL
+    for (i in c(1:length(strata_vars))) {
+      if (i == 1) st1 <- paste0("samp_frame$stratum_new <- cbind(samp_frame[,'",strata_vars[i],"']")
+      if (i > 1 & i != length(strata_vars)) st1 <- paste0(",samp_frame[,'",strata_vars[i],"']")
+      if (i == length(strata_vars)) st1 <- paste0(",samp_frame[,'",strata_vars[i],"'])")
+      st <- paste0(st,st1)
+    }
+    eval(parse(text=st))
+  }
   two <- input_to_beat.2st_2(psu,
                              psu_id=id_PSU,
                              stratum_var=strata_vars,
