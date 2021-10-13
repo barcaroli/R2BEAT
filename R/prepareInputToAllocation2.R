2bc143655890a7cc0d47adfd5ee6d82a01c1db2f
Giulio Barcaroli <gbarcaroli@gmail.com>
2021-10-13 19:03:52 +0200

    new

--- R/prepareInputToAllocation2.R
+++ R/prepareInputToAllocation2.R
@@ -15,10 +15,10 @@ prepareInputToAllocation2 <- function(
   RGcal, 
   id_PSU, 
   id_SSU, 
-  strata_vars, 
+  strata_var, 
   target_vars, 
-  deff_vars, 
-  domain_vars,
+  deff_var, 
+  domain_var,
   delta,
   minimum
 ) 
@@ -27,27 +27,27 @@ prepareInputToAllocation2 <- function(
                              RGcal, 
                              id_PSU, 
                              id_SSU, 
-                             strata_vars, 
+                             strata_vars=strata_var, 
                              target_vars, 
-                             deff_vars, 
-                             domain_vars)
+                             deff_vars=deff_var, 
+                             domain_vars=domain_var)
   samp_frame$one <- 1
   psu <- eval(parse(text=paste0("aggregate(one~",id_PSU,"+",strata_vars,",data=samp_frame,FUN=sum)")))
   mos_var <- "one"
-  if (length(strata_vars) == 1) st <- paste0("samp_frame$stratum_new <- samp_frame$",strata_vars[1],"")
-  if (length(strata_vars) > 1) {
+  if (length(strata_var) == 1) st <- paste0("samp_frame$stratum_new <- samp_frame$",strata_var[1],"")
+  if (length(strata_var) > 1) {
     st <- NULL
-    for (i in c(1:length(strata_vars))) {
-      if (i == 1) st1 <- paste0("samp_frame$stratum_new <- cbind(samp_frame[,'",strata_vars[i],"']")
-      if (i > 1 & i != length(strata_vars)) st1 <- paste0(",samp_frame[,'",strata_vars[i],"']")
-      if (i == length(strata_vars)) st1 <- paste0(",samp_frame[,'",strata_vars[i],"'])")
+    for (i in c(1:length(strata_var))) {
+      if (i == 1) st1 <- paste0("samp_frame$stratum_new <- cbind(samp_frame[,'",strata_var[i],"']")
+      if (i > 1 & i != length(strata_var)) st1 <- paste0(",samp_frame[,'",strata_var[i],"']")
+      if (i == length(strata_var)) st1 <- paste0(",samp_frame[,'",strata_var[i],"'])")
       st <- paste0(st,st1)
     }
     eval(parse(text=st))
   }
   two <- input_to_beat.2st_2(psu,
                              psu_id=id_PSU,
-                             stratum_var=strata_vars,
+                             stratum_var=strata_var,
                              mos_var,
                              delta,
                              minimum)
