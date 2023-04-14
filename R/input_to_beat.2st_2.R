d89cfa847f14fc6581ac43e9cc80c21bbf002043
Giulio Barcaroli <gbarcaroli@gmail.com>
2023-04-14 18:00:10 +0200

    old

--- R/input_to_beat.2st_2.R
+++ R/input_to_beat.2st_2.R
@@ -6,7 +6,7 @@ input_to_beat.2st_2 <- function(psu,psu_id,stratum_var,mos_var,delta,minimum) {
   psu_file$PSU_MOS <- psu[,paste0(mos_var)]
   psu_file <- as.data.frame(psu_file)
   
-  des_file <- aggregate(PSU_MOS ~ stratum_var, data=psu_file,sum)
+  des_file <- aggregate(PSU_MOS ~ STRATUM, data=psu_file,sum)
   des_file$DELTA <- delta
   des_file$MINIMUM <- minimum
   colnames(des_file)[2] <- "STRAT_MOS"
