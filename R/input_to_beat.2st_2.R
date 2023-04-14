375c5ca839698fb5bf7ebe91b06fca9325921524
Giulio Barcaroli <gbarcaroli@gmail.com>
2023-04-14 17:51:09 +0200

    new

--- R/input_to_beat.2st_2.R
+++ R/input_to_beat.2st_2.R
@@ -1,12 +1,3 @@
-#####################################
-# Prepare inputs for R2BEAT::beat.2st
-# Author: Giulio Barcaroli
-#####################################
-# 1. psu_file
-# 2. des_file
-#####################################
-
-
 input_to_beat.2st_2 <- function(psu,psu_id,stratum_var,mos_var,delta,minimum) {
   
   psu_file <- NULL
@@ -15,12 +6,10 @@ input_to_beat.2st_2 <- function(psu,psu_id,stratum_var,mos_var,delta,minimum) {
   psu_file$PSU_MOS <- psu[,paste0(mos_var)]
   psu_file <- as.data.frame(psu_file)
   
-  des_file <- aggregate(PSU_MOS ~ STRATUM, data=psu_file,sum)
+  des_file <- aggregate(PSU_MOS ~ stratum_var, data=psu_file,sum)
   des_file$DELTA <- delta
   des_file$MINIMUM <- minimum
   colnames(des_file)[2] <- "STRAT_MOS"
   out <- list(psu_file=psu_file,des_file=des_file)
   return(out)
 }
-
-
