4dca26a8eb40b93af056ccc9a2d37ea5071378fa
Giulio Barcaroli <gbarcaroli@gmail.com>
2023-04-14 12:19:33 +0200

    new

--- R/select_SSU.R
+++ R/select_SSU.R
@@ -1,59 +1,45 @@
-select_SSU <- function (df, PSU_code, SSU_code, PSU_sampled, verbose = TRUE) 
-{
-  select <- function(df, PSU_code, PSU_sampled) {
-    st <- paste0("PSU_ID <- df$", PSU_code, "[1]")
-    eval(parse(text = st))
-    st <- paste0("PSU_size <- PSU_sampled$PSU_final_sample_unit[PSU_sampled$PSU_ID==df$", 
-                 PSU_code, "[1]]")
-    eval(parse(text = st))
-    SSUs <- unique(df$id_hh)
-    s <- sample(c(1:length(unique(df$id_hh))), size = PSU_size)
-    SSUs_sampled <- SSUs[s]
-    s <- df[df$id_hh %in% SSUs_sampled, ]
-    st <- paste0("s$Pik <- PSU_sampled$Pik[PSU_sampled$PSU_ID==df$",PSU_code, "[1]]")
-    eval(parse(text = st))
-    s$Prob <- nrow(s)/nrow(df)
-    if (verbose == TRUE) {
-      cat("\nPSU = ",as.character(PSU_ID)," *** Selected SSU = ",length(SSUs_sampled)," *** Selected Units = ",nrow(s))
-    }
-    return(s)
+select_SSU <- function (df, PSU_code, SSU_code, PSU_sampled) {
+  require(sampling)
+  frame <- df
+  eval(parse(text=paste0("frame$key <- paste(frame$",PSU_code,",frame$",SSU_code,",sep='*')")))
+  eval(parse(text=paste0("df <- df[df$",PSU_code," %in% PSU_sampled$PSU_ID,c(PSU_code,SSU_code)]")))
+  df$keynew <- c(1:nrow(df))
+  eval(parse(text=paste0("test <- length(unique(df$keynew)) > length(unique(df$",SSU_code,"))")))
+  if (test) {
+    eval(parse(text=paste0("df2 <- df[!duplicated(df$",SSU_code,"),]")))
   }
-  PSU_size <- NULL
-  PSU_ID <- NULL
-  df.split <- NULL
-  a <- NULL
-  st <- paste0("df <- df[df$", PSU_code, " %in% PSU_sampled$PSU_ID, ]")
-  eval(parse(text = st))
-  st <- paste0("df$municipality <- as.factor(df$", PSU_code, 
-               ")")
-  eval(parse(text = st))
-  st <- paste0("df.split <- split(df,df$", PSU_code, ",drop=TRUE)")
-  eval(parse(text = st))
-  samp <- sapply(df.split, function(df) select(df, PSU_code, 
-                                               PSU_sampled))
-  samp <- data.frame(t(samp))
-  st <- paste0(SSU_code, " <- unlist(samp$", SSU_code, ")")
-  eval(parse(text = st))
-  Pik <- unlist(samp$Pik)
-  Prob <- unlist(samp$Prob)
-  st <- paste0("a <- as.data.frame(list(", SSU_code, ",Pik,Prob))")
-  eval(parse(text = st))
-  st <- paste0("colnames(a) <- c('", SSU_code, "','Prob_1st','Prob_2st')")
-  eval(parse(text = st))
-  st <- paste0("a <- a[!duplicated(a$", SSU_code, "),]")
-  eval(parse(text = st))
-  st <- paste0("SSUs <- unique(a$", SSU_code, ")")
-  eval(parse(text = st))
-  st <- paste0("df <- df[df$", SSU_code, " %in% SSUs,]")
-  eval(parse(text = st))
-  st <- paste0("samp <- merge(df, a, by='", SSU_code, "')")
-  eval(parse(text = st))
-  samp$Prob_tot <- samp$Prob_1st * samp$Prob_2st
-  samp$weight <- 1/samp$Prob_tot
+  if (!test) {
+    df2 <- df
+  }
+  eval(parse(text=paste0("df2 <- df2[order(df2$",PSU_code,"),]")))
+  PSU_sampled <- PSU_sampled[order(PSU_sampled$PSU_ID),]
+  s <- strata(df2,stratanames=PSU_code,size=PSU_sampled$PSU_final_sample_unit,method="srswor")
+  samp <- getdata(df2,s)
+  samp$keynew <- NULL
+  if (test) {
+    eval(parse(text=paste0("samp$key <- paste(samp$",PSU_code,",samp$",SSU_code,",sep='*')")))
+    eval(parse(text=paste0("s <- frame[frame$key %in% samp$key, c('",PSU_code,"','",SSU_code,"')]")))
+    samp <- merge(s,samp,all.x=TRUE)
+    samp$key <- NULL
+  }
+  eval(parse(text=paste0("samp <- merge(samp, PSU_sampled,by.x='",PSU_code,"',by.y='PSU_ID')")))
+  if (test) {
+    samp$weight_2st <- samp$weight <- NULL
+    samp$ones <- 1
+    eval(parse(text=paste0("a <- aggregate(ones~",PSU_code,",data=samp,FUN=sum)")))
+    df$ones <- 1
+    eval(parse(text=paste0("b <- aggregate(ones~",PSU_code,",data=df,FUN=sum)")))
+    c <- cbind(a,b[,2])
+    c$weight_2st <- c[,3] / c[,2]
+    samp <- merge(samp,c[,c(1,4)])
+    samp$ones <- NULL
+  }
+  samp$Prob <- samp$ID_unit <- samp$Stratum <- NULL
+  samp$weight <- samp$weight_1st * samp$weight_2st
   cat("\n--------------------------------")
-  cat("\nTotal PSU = ", nrow(PSU_sampled))
-  eval(parse(text=paste0("cat('\nTotal SSU = ', length(unique(samp$", SSU_code, ")))")))
-  cat("\nTotal Units = ", nrow(samp))
+  cat("\nTotal PSUs = ", nrow(PSU_sampled))
+  eval(parse(text = paste0("cat('\nTotal SSUs = ', length(unique(samp$", SSU_code, ")))")))
+  if (test) cat("\nTotal Units = ", nrow(samp))
   cat("\n--------------------------------")
-  return(samp)
+  samp
 }
