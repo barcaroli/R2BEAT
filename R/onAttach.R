543a8e84b59cdb20215e0206b320565f780a6b1d
Giulio Barcaroli <gbarcaroli@gmail.com>
2023-05-19 11:22:51 +0200

    onAttach

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,6 +1,6 @@
 .onAttach<-function(libname, pkgname){
   packageStartupMessage("\n", appendLF = FALSE)
-  packageStartupMessage("Please install ReGenesees: devtools::install_github('DiegoZardetto/ReGenesees')", appendLF = FALSE)
+  packageStartupMessage("Please install ReGenesees --> devtools::install_github('DiegoZardetto/ReGenesees')", appendLF = FALSE)
   packageStartupMessage("\n", appendLF = FALSE)
   packageStartupMessage("Report issues at https://github.com/barcaroli/R2BEAT/issues", appendLF = FALSE)
   packageStartupMessage("\n", appendLF = FALSE)
