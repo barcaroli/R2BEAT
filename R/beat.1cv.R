3664def93a436e4e00ac24b554a0a1f84ff16623
Giulio Barcaroli <gbarcaroli@gmail.com>
2024-04-08 11:52:25 +0200

    new

--- R/beat.1cv.R
+++ R/beat.1cv.R
@@ -1,14 +1,14 @@
 #' The function returns a dataframe with planned and actual coefficients of variation (CV) in a multivariate multi-domain allocation problem.
-#' 
+#'
 #' @param stratif A dataframe with strata information.
 #' @param errors: A dataframe with planned CV.
 #' @param alloc: A vector with an allocation into strata.
 #' @param minnumstrat: Optional. A number indicating the lower bound for the number of allocated units in each stratum (default is 2).
 #' @return A dataframe with planned and actual CV for each combination of domain, domain category and auxiliary variable.
-# ----------------------------------------------------------------------
 beat.1cv <- function(stratif, errors,alloc, minnumstrat = 2) 
 {
 
+  
   # Begin body
   # First input data frame
   colnames(stratif) <- toupper(colnames(stratif))
@@ -17,14 +17,73 @@ beat.1cv <- function(stratif, errors,alloc, minnumstrat = 2)
   colnames(errors) <- toupper(colnames(errors))
   #
   #check inputs:
+  # functions 
+  checkData <- function(strata = NULL, errors = NULL) {
+    # controls on strata dataframe
+    if (!is.null(strata)) {
+      if (sum(grepl("N", colnames(strata))) < 1) 
+        stop("In strata dataframe the indication of population (N) is missing")
+      
+      if (sum(grepl("STRAT", toupper(colnames(strata)), fixed = TRUE)) < 
+          1) 
+        stop("In strata dataframe the indication of stratum (STRATUM) is missing")
+      if (sum(grepl("DOM1", toupper(colnames(strata)), fixed = TRUE)) < 
+          1) 
+        stop("In strata dataframe the indication of at least Ine domain (DOM1) is missing")
+      if(sum(grepl('CENS',toupper(colnames(strata)),fixed=TRUE))
+         < 1) stop('In strata dataframe the indication of strata
+    to be sampled or censused (CENS) is missing') 
+      if(sum(grepl('COST',toupper(colnames(strata)),fixed=TRUE))
+         < 1) stop('In strata dataframe the indication of
+    interviewing cost in strata (COST) is missing')
+      if (sum(grepl("M1", toupper(colnames(strata)), fixed = TRUE)) < 1) 
+        stop("In strata dataframe the indication of at least one mean (M1) is missing")
+      if (sum(grepl("S1", toupper(colnames(strata)), fixed = TRUE)) < 1) 
+        stop("In strata dataframe the indication of at least one standard deviation (S1) is missing")
+      if(sum(grepl('^M+[0123456789]',toupper(colnames(strata)),perl=TRUE))!=  sum(grepl('^S+[0123456789]',toupper(colnames(strata)),perl=TRUE)))
+        stop('In strata dataframe the number of means (Mx)
+    differs from the number of standard deviations (Sx)')
+    }
+    # controls on errors dataframe
+    if (!is.null(errors)) {
+      if (sum(grepl("DOM", toupper(colnames(errors)), fixed = TRUE)) < 
+          1) 
+        stop("In errors dataframe the indication of domain (DOM) is missing")
+      if (sum(grepl("CV", toupper(colnames(errors)), fixed = TRUE)) < 
+          1) 
+        stop("In errors dataframe the indication of at least one constraint (CV) is missing")
+    }
+    # crossed controls between errors and strata
+    if (!is.null(errors) && !is.null(strata)) {
+      if
+      (sum(grepl('^S+[0123456789]',toupper(colnames(strata)),perl=TRUE)) != sum(grepl('CV',toupper(colnames(errors)),fixed=TRUE)))
+        stop('In strata dataframe the number of means and std deviations differs from the number of coefficient of
+    variations in errors dataframe')
+      if (sum(grepl("DOM", toupper(colnames(strata)), fixed = TRUE)) !=   nrow(errors)) 
+        stop("The different domains (DOMx) in strata dataframe are not represented in errors dataframe")
+    }
+  }
+  
+  # check n<minnumstr
+  check_n <- function(allocation=NULL, strata=NULL, minnumstrat) {
+    if (!is.null(allocation)) {
+      if(any(allocation<minnumstrat & strata$N>minnumstrat)){
+        stop(paste("The allocated sample size is lower than the required minimum number of sampled unit per stratum, even if the stratum size allows to select a higher size. This happens in the following strata: ", paste(strata$STRATUM[which(n<minnumstrat & strata$N>minnumstrat)], "\n", sep="")))
+      }
+      if(any(strata$CENS==1 & strata$N!=allocation)){
+        stop(paste("The stratum ", strata$STRATUM[which(strata$CENS==1 & strata$N!=n)], " must be censused. \n", sep=""))
+      }
+      
+    }
+  }
   #
   checkData(strata = stratif, errors = errors)
   #
   check_n(allocation=alloc, strata=stratif, minnumstrat)
   #
   ordina_variabili <- function(dati, prefisso, n_var) {
-    #order df columns,
-    #s.t. we have prefix_1_var_1, prefix_1_var_2, ..., prefix_n_var_1, prefix_n_var_n
+    #ordina le colonne del df,
+    #in modo tale da avere prefisso1,...prefisson_var
     if (!is.data.frame(dati)) 
       stop()
     as.matrix(dati[, paste(prefisso, 1:n_var, sep = ""), 
@@ -131,8 +190,8 @@ beat.1cv <- function(stratif, errors,alloc, minnumstrat = 2)
   # different domains of interest
   # -------------------------------------------------------------
  
-  cvDom <- NULL  #to be printed
-  cvDom2 <- NULL  #to be printed
+  cvDom <- NULL  #Per stampa
+  cvDom2 <- NULL  #per stampa
   for (k in 1:ndom) {
     cvx <- ordina_variabili(errors[k, ], "CV", nvar)
     ndomvalues <- c(1:nvalues[k])
@@ -146,64 +205,22 @@ beat.1cv <- function(stratif, errors,alloc, minnumstrat = 2)
   
   calcola_cv <- function() {
     
+    NTOT <- c(rep(0, nvar))
+    CVfin <- c(rep(0, nvar))
     #
     # -------------------------------------------------------------
     # Populations in strata for different domains of interest
     # NTOTj
     # -------------------------------------------------------------
     NTOT <- colSums((m > 0) * N)
-    # NTOT is a row-vector of dimension equal to the number of column of m (or s)
-    # with the total number of population units in each domain
-    NTOT_all=m
-    NTOT_all=t(apply(NTOT_all, 1, function(x) x=t(as.numeric(x>0)*NTOT)))
-    
-    # NTOT_all is a matrix with the same dimension as m: 
-    #1) NTOT_all has been inizialed as equal to m 
-    #2) Then in each column j, the non-null value will be filled up with  
-    #the j-the element of vector NTOT
     #
-    # -------------------------------------------------------------
-    # Sampled population in strata for different domains of interest
-    # allocTOTj
-    # -------------------------------------------------------------
-    allocTOT=colSums((m > 0) * round(alloc))
-    # allocTOT is a row-vector of dimension equal to the number of column of m (or s)
-    # with the total number of sampled units in each domain
-    #
-    
     # ------------------------------------------------------------
     # Computation of the CVs
     # ------------------------------------------------------------
-    #
-    # ------------------------------------------------------------
-    # Total
-    # ------------------------------------------------------------ 
+    varfin <- rowSums(t((s * N)^2 * (1 - round(alloc)/N)/round(alloc))/NTOT^2)
     totm <- rowSums(t(m * N))
-    # row-vector of dimension equal to the number of column of m (or s)
-    # with the total (mean*number of pop units) in each domain 
-    totm_all=m
-    totm_all=t(apply(totm_all, 1, function(x) x=t(as.numeric(x>0)*t(totm))))
-    # totm_all is a matrix with the same dimension as m: 
-    #1) totm_all has been inizialed as equal to m 
-    #2) Then in each column j, the non-null value will be filled up with  
-    #the j-the element of vector totm
     
-    # ------------------------------------------------------------
-    # Variance
-    # ------------------------------------------------------------ 
-    var=s^2*(N-1)
-    #correction factor
-    diff=N*((1/N)*(m*N) - totm_all/NTOT_all)^2
-    diff[is.na(diff)]=0
-    var2=sqrt((colSums(var)+colSums(diff))/NTOT) 
-    varfin= var2^2*((1 - allocTOT/NTOT)*1/(allocTOT))
-
-    # ------------------------------------------------------------
-    # CV
-    # ------------------------------------------------------------ 
-    
-    CVfin <- sqrt(varfin/(totm/NTOT)^2)
-    #sd/mean
+    CVfin <- round(sqrt(varfin/(totm/NTOT)^2), digits = 4)
     return(CVfin)
   }
   
@@ -224,64 +241,10 @@ beat.1cv <- function(stratif, errors,alloc, minnumstrat = 2)
   return(out_cv)
   
   # End body
+  
+  
+  
 }
 
-checkData <- function(strata = NULL, errors = NULL) {
-  # controls on strata dataframe
-  if (!is.null(strata)) {
-    if (sum(grepl("N", colnames(strata))) < 1) 
-      stop("In strata dataframe the indication of population (N) is missing")
-    
-    if (sum(grepl("STRAT", toupper(colnames(strata)), fixed = TRUE)) < 
-        1) 
-      stop("In strata dataframe the indication of stratum (STRATUM) is missing")
-    if (sum(grepl("DOM1", toupper(colnames(strata)), fixed = TRUE)) < 
-        1) 
-      stop("In strata dataframe the indication of at least Ine domain (DOM1) is missing")
-    if(sum(grepl('CENS',toupper(colnames(strata)),fixed=TRUE))
-    < 1) stop('In strata dataframe the indication of strata
-    to be sampled or censused (CENS) is missing') 
-    if(sum(grepl('COST',toupper(colnames(strata)),fixed=TRUE))
-    < 1) stop('In strata dataframe the indication of
-    interviewing cost in strata (COST) is missing')
-    if (sum(grepl("M1", toupper(colnames(strata)), fixed = TRUE)) < 1) 
-      stop("In strata dataframe the indication of at least one mean (M1) is missing")
-    if (sum(grepl("S1", toupper(colnames(strata)), fixed = TRUE)) < 1) 
-      stop("In strata dataframe the indication of at least one standard deviation (S1) is missing")
-    if(sum(grepl('^M+[0123456789]',toupper(colnames(strata)),perl=TRUE))!=  sum(grepl('^S+[0123456789]',toupper(colnames(strata)),perl=TRUE)))
-    stop('In strata dataframe the number of means (Mx)
-    differs from the number of standard deviations (Sx)')
-  }
-  # controls on errors dataframe
-  if (!is.null(errors)) {
-    if (sum(grepl("DOM", toupper(colnames(errors)), fixed = TRUE)) < 
-        1) 
-      stop("In errors dataframe the indication of domain (DOM) is missing")
-    if (sum(grepl("CV", toupper(colnames(errors)), fixed = TRUE)) < 
-        1) 
-      stop("In errors dataframe the indication of at least one constraint (CV) is missing")
-  }
-  # crossed controls between errors and strata
-  if (!is.null(errors) && !is.null(strata)) {
-    if
-    (sum(grepl('^S+[0123456789]',toupper(colnames(strata)),perl=TRUE)) != sum(grepl('CV',toupper(colnames(errors)),fixed=TRUE)))
-    stop('In strata dataframe the number of means and std deviations differs from the number of coefficient of
-    variations in errors dataframe')
-    if (sum(grepl("DOM", toupper(colnames(strata)), fixed = TRUE)) !=   nrow(errors)) 
-      stop("The different domains (DOMx) in strata dataframe are not represented in errors dataframe")
-  }
-}
 
-# check n<minnumstr
-check_n <- function(allocation=NULL, strata=NULL, minnumstrat) {
-  if (!is.null(allocation)) {
-    if(any(allocation<minnumstrat & strata$N>minnumstrat)){
-      stop(paste("The allocated sample size is lower than the required minimum number of sampled unit per stratum, even if the stratum size allows to select a higher size. This happens in the following strata: ", paste(strata$STRATUM[which(n<minnumstrat & strata$N>minnumstrat)], "\n", sep="")))
-    }
-    if(any(strata$CENS==1 & strata$N!=allocation)){
-      stop(paste("The stratum ", strata$STRATUM[which(strata$CENS==1 & strata$N!=n)], " must be censused. \n", sep=""))
-    }
-    
-  }
-}
   
---
+++ R/expected_CV_new.R
@@ -0,0 +1,78 @@
+#' expected_CV
+#'  
+#' @description
+#' Function to report the expected coefficients of variation for target variables Ys in a 'strata' dataset 
+#' given an allocation 'alloc' and the current set of precision constraints
+#' 
+#'  
+#' @param strata name of the dataframe containing information in the sampling strata.
+#' @param alloc vector containing the allocation of sampling units.
+#' @param errors name of the dataframe 
+#' 
+#' @return a dataframe containing the maximum expected coefficients of variation in each domain level for each target variable
+#' 
+#' @examples
+#' load("./data/sample.RData")
+#' target_vars <- c("active","inactive","unemployed","income_hh")
+#' strata <- R2BEAT:::prepareInputToAllocation_beat.1st(samp_frame = samp,
+#'                                                      ID = "id_hh",
+#'                                                      stratum = "stratum_label",
+#'                                                      dom = "region",
+#'                                                      target = target_vars)
+#' strata$CENS <- as.numeric(strata$CENS)
+#' strata$COST <- as.numeric(strata$COST)
+#' strata$CENS <- 0
+#' cv <- as.data.frame(list(DOM = c("DOM1","DOM2"),
+#'                          CV1 = c(0.05,0.10),
+#'                          CV2 = c(0.05,0.10),
+#'                          CV3 = c(0.05,0.10),
+#'                          CV4 = c(0.05,0.10)))
+#' allocation <- beat.1st(strata,cv)
+#' 
+#' alloc <- allocation$alloc$ALLOC[-nrow(allocation$alloc)]
+#' exp_cv <- expected_CV(strata,cv,alloc)
+#' exp_cv
+#' 
+
+
+expected_CV <- function(strata,errors,alloc) {
+  data <- beat.1cv(strata,errors,alloc)
+  split_strings <- strsplit(data$`DOMAIN/VAR`, split = "/")
+  data$Dom <- sapply(split_strings, `[`, 1)
+  data$Var <- sapply(split_strings, `[`, 2)
+  data <- data[,c("TYPE","Dom","Var","ACTUAL_CV")]
+  colnames(data) <- c("Type","Dom","Var","Actual CV")
+  ndom <- length(unique(data$Type))
+  nvar <- length(unique(data$Var))
+  unique_combinations <- unique(data[, c("Type", "Dom")])
+  exp_cv  <- as.data.frame(list(Type = unique_combinations[,1],
+                                   DOM = unique_combinations[,2],
+                                   CV1 = rep(0,nrow(unique_combinations))))
+  for (i in c(2:(nvar))) {
+    eval(parse(text=paste0("exp_cv$CV",i," <- 0")))
+  }
+  # Loop through each unique combination
+  for (i in 1:nrow(unique_combinations)) {
+    # Extract the current combination
+    current_combination <- unique_combinations[i, ]
+    exp_cv$Type[i] <- unique_combinations[i,1]
+    exp_cv$DOM[i] <- unique_combinations[i,2]
+    subset_data <- subset(data, Type == current_combination$Type & Dom == current_combination$Dom)
+    for (j in c(1:nvar)) {
+      eval(parse(text=paste0("exp_cv$CV",j,"[",i,"] <- subset_data$`Actual CV`[",j,"]")))
+    }
+  }
+  exp_cv
+  cv_columns <- grep("CV", names(exp_cv), value = TRUE)
+  split_data <- split(exp_cv, exp_cv$Type)
+  calculate_max <- function(data_list) {
+    sapply(cv_columns, function(column_name) {
+      max(data_list[[column_name]], na.rm = TRUE)
+    })
+  }
+  max_values <- lapply(split_data, calculate_max)
+  max_values <- lapply(max_values,function(x) round(x,4))
+  max_values_df <- do.call(rbind, max_values)
+  row.names(max_values_df) <- unique(exp_cv$Type)
+  return(max_values_df)
+}
