7d31280893cde0793165e1c920bb79aebfc802dd
Giulio Barcaroli <gbarcaroli@gmail.com>
2024-05-31 12:46:37 +0200

    staging all files

--- pkgdown.R
+++ pkgdown.R
@@ -10,13 +10,14 @@ require(devtools)
 # library(tvthemes)
 
 
-# devtools::install_github("hadley/pkgdown")
+# devtools::install_github("hadley/pkgdown", version=1.6.1)
 setwd("D:/Google Drive/Sampling/R2BEAT/R2BEAT_1.0.6/")
 # usethis::use_github_action("pkgdown")
 # setwd("C:\\Users\\UTENTE\\Google Drive\\Sampling\\R2BEAT\\R2BEAT_1.0.5")
 library(pkgdown)
 # usethis::use_pkgdown()
 # Sys.setenv(R_WIN_USE_SAFE_CALLBACKS = "0")
+init_site(pkg = ".")
 devtools::build_site()
 
 
