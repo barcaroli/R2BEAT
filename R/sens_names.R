4cbad8f413bf235d78a47f89730ef343602c9a7a
Giulio Barcaroli <gbarcaroli@gmail.com>
2025-10-11 11:02:05 +0200

    sens_names

--- R/sens_names.R
+++ R/sens_names.R
@@ -1,5 +1,5 @@
 sens_names <- function(s,target_vars,strata) {
-  s$Var <- target_vars
+  # s$Var <- target_vars
   s$Dom[s$Type=="DOM1"] <- "Total"
   ndom <- sum(grepl("DOM",colnames(strata))) - 1
   for (j in c(1:ndom)) {
@@ -9,5 +9,6 @@ sens_names <- function(s,target_vars,strata) {
     eval(parse(text=paste0("for (i in c(1:length(levels(strata$DOM",j+1,")))) t[t==i] <- levels(strata$DOM",j+1,")[i]")))
     eval(parse(text=paste0("s$Dom[s$Type=='DOM",j+1,"'] <- t")))
   }
+  colnames(s)[c(3:ncol(s))] <- target_vars
   return(s)
 }
